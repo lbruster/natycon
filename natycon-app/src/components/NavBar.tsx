8d1017cc0c83c8c344ac183881f8ac3f12712930
Leroy B <shoked@gmail.com>
2025-10-20 20:57:46 -0500

    Start Hero

--- natycon-app/src/components/NavBar.tsx
+++ natycon-app/src/components/NavBar.tsx
@@ -4,9 +4,14 @@ import NavLogo from "/images/nav-logo.svg";
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
-      <img src={NavLogo} alt="nav-logo" className="md:w-24 w-20" />
-    </nav>
+    <>
+      <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
+        <img src={NavLogo} alt="nav-logo" className="md:w-24 w-20" />
+      </nav>
+      <nav className="fixed top-0 right-0 z-50 md:p-9 p-3">
+        <img src={NavLogo} alt="nav-logo" className="md:w-24 w-20" />
+      </nav>
+    </>
   );
 };
 
